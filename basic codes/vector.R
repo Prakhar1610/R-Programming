55bf83218c7754961887cfd405f22f413de54b72
Prakhar1610 <149139421+Prakhar1610@users.noreply.github.com>
2024-03-06 13:08:38 +0530

    Update vector.R

--- basic codes/vector.R
+++ basic codes/vector.R
@@ -1,3 +1,11 @@
+#                              Vector
+#Vector are of 2 types
+#1- Atomic Vector
+#2 - List
+
+
+#Atomic Vector - it store values of only one type of datatype
+#List - it store values of multiple type of datatype
 p <- c("Prakhar" , "Aditya" , "Shivam")
 if("good" %in% p){
   print("present")
