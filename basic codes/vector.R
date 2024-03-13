63ff884601e7d9a3519326f6b39040f77ad372e3
Prakhar1610 <149139421+Prakhar1610@users.noreply.github.com>
2024-03-13 10:14:50 +0530

    Update vector.R

--- basic codes/vector.R
+++ basic codes/vector.R
@@ -14,7 +14,7 @@ if("good" %in% p){
 }
 
 
-#ATOMIC VECTOR
+#                                  ATOMIC VECTOR
 a<-c(1,3,3,4,6)
 print(a)
 
@@ -111,3 +111,127 @@ a2[2,4] #Error in a2[2, 4] : incorrect number of dimensions
 
 #this is right
 a2[c(2,4)] #output - "kumar"    "tripathi"
+
+
+#naming in vector
+
+n<-c("ram","kumar","gupta","tripathi")
+n    #output - "ram"      "kumar"    "gupta"    "tripathi"
+names(n) = c("y1","y2","y3")
+n["y2"]   #output -  y2 
+          #     "kumar"
+
+
+
+
+#                                    LIST
+ver<-c(8,9,4,7,6)
+char_vec<-c("ram","shyam","mohan","sohan","rohan")
+logic_vec<-c(TRUE,FALSE,FALSE,TRUE,FALSE)
+list1<-list(ver , char_vec , logic_vec)
+list1
+#output
+# 8 9 4 7 6
+# "ram"   "shyam" "mohan" "sohan" "rohan"
+# TRUE FALSE FALSE  TRUE FALSE
+
+list1[1] #output - 8 9 4 7 6
+list1[2] #output - "ram"   "shyam" "mohan" "sohan" "rohan"
+list1[3] #output - TRUE FALSE FALSE  TRUE FALSE
+
+
+list2<-list(c(8,9,4,7,6) , c("ram","shyam","mohan","sohan","rohan") , c(TRUE,FALSE,FALSE,TRUE,FALSE))
+list2
+#output
+# 8 9 4 7 6
+# "ram"   "shyam" "mohan" "sohan" "rohan"
+# TRUE FALSE FALSE  TRUE FALSE
+
+list2<-list(8,9,4,7,6,"ram","shyam","mohan","sohan","rohan",TRUE,FALSE,FALSE,TRUE,FALSE)
+list2
+
+
+list3<-list(5:9)
+list3 #output - 5 6 7 8 9
+class(list3) #output - "list"
+
+list4<-list(14:19)
+list4
+
+v1<-unlist(list3)
+v1
+class(v1) #output - "integer"
+
+v2<-unlist(list(5.6,7.99,88.09))
+v2
+class(v2) #output - "numeric"
+
+v3<-unlist(list4)
+class(v3) #output - "integer"
+
+v4<-list('a','e')
+v4<-unlist(v4)
+class(v4) #output - "character"
+
+list3<-list(5:9)
+list4<-list(14:19)
+mer<-list(list3,list4) #it will merge both of the list
+mer
+
+
+#                            List arithematics
+v1 <- c(3,8,4,5,0,11)
+v2 <- c(4,11,0,8,1,2)
+
+# Vector addition.
+add.result <- v1+v2
+print(add.result)#Output - 7 19  4 13  1 13
+
+# Vector subtraction.
+sub.result <- v1-v2
+print(sub.result)#Output -  -1 -3  4 -3 -1  9
+
+# Vector multiplication.
+multi.result <- v1*v2
+print(multi.result) #Output - 12 88  0 40  0 22
+
+# Vector division.
+divi.result <- v1/v2
+print(divi.result)#Output - 0.7500000 0.7272727       Inf 0.6250000 0.0000000 5.5000000
+
+
+
+
+#                        Vector Element Recycling
+v1 <- c(3,8,4,5,0,11)
+v2 <- c(4,11)
+# V2 becomes c(4,11,4,11,4,11)
+
+add.result <- v1+v2
+print(add.result)# Output - 7 19  8 16  4 22
+
+sub.result <- v1-v2
+print(sub.result)# Output - -1 -3  0 -6 -4  0
+
+
+
+
+#                       Vector Element Sorting
+v <- c(3,8,4,5,0,11, -9, 304)
+
+# Sort the elements of the vector.
+sort.result <- sort(v)
+print(sort.result) #OUTPUT - [1]  -9   0   3   4   5   8  11 304
+
+# Sort the elements in the reverse order.
+revsort.result <- sort(v, decreasing = TRUE)
+print(revsort.result) #OUTPUT - [1] 304  11   8   5   4   3   0  -9
+
+# Sorting character vectors.
+v <- c("Red","Blue","yellow","violet")
+sort.result <- sort(v)
+print(sort.result) #OUTPUT - [1] "Blue"   "Red"    "violet" "yellow"
+
+# Sorting character vectors in reverse order.
+revsort.result <- sort(v, decreasing = TRUE)
+print(revsort.result) #OUTPUT - [1] "yellow" "violet" "Red"    "Blue"  
