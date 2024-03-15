ae10feff5acb08b79a3a8938d0e8175348e73d29
Prakhar1610 <149139421+Prakhar1610@users.noreply.github.com>
2024-03-16 00:30:05 +0530

    Update Arrays.R

--- basic codes/Arrays.R
+++ basic codes/Arrays.R
@@ -123,6 +123,21 @@ row.names1 <- c("ROW1","ROW2","ROW3")
 matrix.names1 <- c("Matrix1","Matrix2")
 a1<-array(c(v1,v2) , dim=c(3,3,2) ,dimnames=list(row.names1,column.names1,matrix.names1))
 print(a1)
+#Output-
+# , , Matrix1
+# 
+#      COL1 COL2 COL3
+# ROW1    1    4    7
+# ROW2    2    5    8
+# ROW3    3    6    9
+# 
+# , , Matrix2
+# 
+#      COL1 COL2 COL3
+# ROW1   10    1    4
+# ROW2   11    2    5
+# ROW3   12    3    6
+
 
 v3<-c(1,2,3)
 v4<-c(4,5,6,7,8,9,10,11,12)
@@ -131,6 +146,21 @@ row.names2 <- c("ROW1","ROW2","ROW3")
 matrix.names2 <- c("Matrix1","Matrix2")
 a2<-array(c(v1,v2) , dim=c(3,3,2) ,dimnames=list(row.names2,column.names2,matrix.names2))
 print(a2)
+#Output-
+# , , Matrix1
+# 
+#      COL1 COL2 COL3
+# ROW1    1    4    7
+# ROW2    2    5    8
+# ROW3    3    6    9
+# 
+# , , Matrix2
+# 
+#      COL1 COL2 COL3
+# ROW1   10    1    4
+# ROW2   11    2    5
+# ROW3   12    3    6
+
 
 a3<-a1+a2 #Dimensions of both the arrays should be same
 print(a3)
@@ -148,3 +178,178 @@ print(a3)
 # ROW1   20    2    8
 # ROW2   22    4   10
 # ROW3   24    6   12
+
+a4 <- a1-a2
+print(a4)
+#Output -
+# , , Matrix1
+# 
+#     COL1 COL2 COL3
+# ROW1    0    0    0
+# ROW2    0    0    0
+# ROW3    0    0    0
+# 
+# , , Matrix2
+# 
+#      COL1 COL2 COL3
+# ROW1    0    0    0
+# ROW2    0    0    0
+# ROW3    0    0    0
+
+
+a5 <- a1*a2
+print(a5)
+#Output
+# , , Matrix1
+# 
+#      COL1 COL2 COL3
+# ROW1    1   16   49
+# ROW2    4   25   64
+# ROW3    9   36   81
+# 
+# , , Matrix2
+# 
+#      COL1 COL2 COL3
+# ROW1  100    1   16
+# ROW2  121    4   25
+# ROW3  144    9   36
+
+
+a6 <- a1/a2
+print(a6)
+#Output
+# , , Matrix1
+# 
+#      COL1 COL2 COL3
+# ROW1    1    1    1
+# ROW2    1    1    1
+# ROW3    1    1    1
+# 
+# , , Matrix2
+# 
+#      COL1 COL2 COL3
+# ROW1    1    1    1
+# ROW2    1    1    1
+# ROW3    1    1    1
+
+
+# %% - modulus operator
+a7 <- a1%%a2
+print(a7)
+#Output
+# , , Matrix1
+# 
+#      COL1 COL2 COL3
+# ROW1    0    0    0
+# ROW2    0    0    0
+# ROW3    0    0    0
+# 
+# , , Matrix2
+# 
+#      COL1 COL2 COL3
+# ROW1    0    0    0
+# ROW2    0    0    0
+# ROW3    0    0    0
+
+
+#                         Manipulating Array Elements
+# Create two vectors of different lengths.
+vector1 <- c(5,9,3)
+vector2 <- c(10,11,12,13,14,15)
+
+# Take these vectors as input to the array.
+arr1 <- array(c(vector1,vector2),dim = c(3,3,2))
+arr1
+#OUTPUT -
+# , , 1
+# 
+#      [,1] [,2] [,3]
+# [1,]    5   10   13
+# [2,]    9   11   14
+# [3,]    3   12   15
+# 
+# , , 2
+# 
+#      [,1] [,2] [,3]
+# [1,]    5   10   13
+# [2,]    9   11   14
+# [3,]    3   12   15
+
+# Create two vectors of different lengths.
+vector3 <- c(9,1,0)
+vector4 <- c(6,0,11,3,14,1,2,6,9)
+arr2 <- array(c(vector3,vector4),dim = c(3,3,2))
+arr2
+#Output-
+# , , 1
+# 
+#      [,1] [,2] [,3]
+# [1,]    9    6    3
+# [2,]    1    0   14
+# [3,]    0   11    1
+# 
+# , , 2
+# 
+#      [,1] [,2] [,3]
+# [1,]    2    9    6
+# [2,]    6    1    0
+# [3,]    9    0   11
+
+# create matrices from these arrays.
+matrix1 <- arr1[,,2]
+matrix1
+#      [,1] [,2] [,3]
+# [1,]    5   10   13
+# [2,]    9   11   14
+# [3,]    3   12   15
+
+matrix2 <- arr2[,,2]
+matrix2
+#      [,1] [,2] [,3]
+# [1,]    2    9    6
+# [2,]    6    1    0
+# [3,]    9    0   11
+
+
+# Add the matrices.
+result <- matrix1+matrix2
+print(result)
+#OUTPUT
+#      [,1] [,2] [,3]
+# [1,]    7   19   19
+# [2,]   15   12   14
+# [3,]   12   12   26
+
+
+
+
+#                          Calculation Across Array Element
+
+# Create two vectors of different lengths.
+vector1 <- c(5,9,3)
+vector2 <- c(10,11,12,13,14,15)
+
+# Take these vectors as input to the array.
+new.array <- array(c(vector1,vector2),dim = c(3,3,2))
+print(new.array)
+#OUTPUT-
+# , , 1
+# 
+#      [,1] [,2] [,3]
+# [1,]    5   10   13
+# [2,]    9   11   14
+# [3,]    3   12   15
+# 
+# , , 2
+# 
+#      [,1] [,2] [,3]
+# [1,]    5   10   13
+# [2,]    9   11   14
+# [3,]    3   12   15
+
+
+# Use apply to calculate the sum of the rows across all the matrices.
+result <- apply(new.array, c(1), sum)
+print(result)
+#OUTPUT
+#[1] 56 68 60
